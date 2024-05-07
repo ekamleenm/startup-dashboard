43cf24a723a58a6b60023508b65eaa76169d4daa
ekamleen <ekamleenm@gmail.com>
2024-05-07 11:27:58 -0700

    UI done

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('startup_funding.csv')
-st.dataframe(df)
+# Data Cleaning -> Missing values should be taken care of:
+df['Investors Name'] = df['Investors Name'].fillna('Undisclosed')
 
 st.sidebar.title('Startup Funding Analysis')
 option = st.sidebar.selectbox('Select One', ['Overall Analysis', 'Startup', 'Investor'])
@@ -10,8 +11,12 @@ option = st.sidebar.selectbox('Select One', ['Overall Analysis', 'Startup', 'Inv
 if option == 'Overall Analysis':
     st.title("Overall Analysis")
 elif option == 'Startup':
-    st.sidebar.selectbox('Select Startup', ['a', 'b', 'c', 'd'])
+    st.sidebar.selectbox('Select Startup', sorted(df['Startup Name'].unique().tolist()))
+    btn1 = st.sidebar.button('Startup Details')
     st.title("Startup Analysis")
+
 else:
-    st.sidebar.selectbox('Select Investor', ['person1', 'person2', 'person3'])
+    st.sidebar.selectbox('Select Investor', sorted(df['Investors Name'].unique().tolist()))
+    btn2 = st.sidebar.button('Investor Details')
     st.title("Investor Analysis")
+
