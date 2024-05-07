0920c44641c9b726ee3b7556f06e0e99274637e0
ekamleen <ekamleenm@gmail.com>
2024-05-06 23:42:46 -0700

    login

--- st_session_2.py
+++ st_session_2.py
@@ -2,11 +2,17 @@ import streamlit as st
 
 email = st.text_input('Enter Email')
 password = st.text_input('Enter Password')
+gender = st.selectbox('Select Gender', ['m', 'f', 'other'])
 
 btn = st.button('Login Here')
 
 if btn:
     if email == 'ekm9@sfu.ca' and password == '1234':
         st.success('Login successful')
+        st.balloons()
+        st.write(gender)
     else:
         st.error('Login failed')
+
+
+# file uploader
