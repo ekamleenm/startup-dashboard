6dd1b7b612bc7411b3238449f38cde3ac5c0ce9b
ekamleen <ekamleenm@gmail.com>
2024-05-08 12:29:17 -0700

    investors

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+
+st.set_page_config(layout='wide', page_title='Startup Analysis')
 
 df = pd.read_csv('startup_cleaned.csv')
 # Data Cleaning -> Missing values should be taken care of:
@@ -8,10 +11,19 @@ df['investors'] = df['investors'].fillna('Undisclosed')
 
 def load_investor_details(investor_):
     st.title(selected_investor)
-#     recent 5 investments of investor
-    last5_df = df[df['investors'].str.contains(investor_)].head(5)[['startup','vertical','city','round','amount']]
-    st.subheader('Recent 5 Investments')
+    #     recent 5 investments of investor
+    last5_df = df[df['investors'].str.contains(investor_)].head(5)[['startup', 'vertical', 'city', 'round', 'amount']]
+    st.subheader('Recent Investments')
     st.dataframe(last5_df)
+    col1, col2 = st.columns(2)
+    with col1:
+        #     Biggest 5 investments
+        big_series = df[df['investors'].str.contains(investor_)].groupby('startup')['amount'].sum().sort_values(
+            ascending=False).head(5)
+        st.subheader('Top Investments')
+        fig, ax = plt.subplots()
+        ax.bar(big_series.index, big_series.values)
+        st.pyplot(fig)
 
 
 st.sidebar.title('Startup Funding Analysis')
@@ -29,6 +41,3 @@ else:
     btn2 = st.sidebar.button('Investor Details')
     if btn2:
         load_investor_details(selected_investor)
-
-
-
